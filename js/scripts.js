920e397ea5387b381282ea7fbf67fef7b8cb4750
Gieryn <gierynt@gmail.com>
2017-10-01 21:42:33 +0200

    JavaScript first task

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// single line comment, mozna tez uzyc jako 
+//chwilowe wylaczenie fragmentu kodu
+
+
+
+/*
+
+multi
+line 
+comment
+
+*/
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name)
